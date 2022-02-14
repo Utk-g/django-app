2ee6a9f124c01d3c4effa8e74d8f5e54ffcba8f6
Utkarsh Gupta <utkarsh1999gupta@gmail.com>
2022-02-14 21:07:44 +0530

    First commit

---
+++ demo/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+
+def hi(request):
+    name=request.GET['name']
+    return render(request,'home.html',{'name':name})
